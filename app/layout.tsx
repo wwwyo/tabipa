b1d896de002f1326b8a9a6e375e744dd3430303b
yuito.watanabe <w.ytmrm11811@gmail.com>
2024-09-21 16:54:18 +0900

    chore: init supabase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,18 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import DeployButton from "@/components/deploy-button";
+import { EnvVarWarning } from "@/components/env-var-warning";
+import HeaderAuth from "@/components/header-auth";
+import { hasEnvVars } from "@/lib/supabase/check-env-vars";
+import { Link } from "lucide-react";
+import { ThemeProvider } from "next-themes";
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
 
 export const metadata: Metadata = {
+  metadataBase: new URL(defaultUrl),
   title: "Tabipa",
   description:
     "旅行のパフォーマンスを最大化し、心に残る最高の思い出作りを「タビパ」がサポートします。旅の隙間をオープンにし、最高の旅を実現しましょう。",
@@ -13,8 +24,49 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
-      <body className={"antialiased"}>{children}</body>
+    <html lang="ja" suppressHydrationWarning>
+      <body className="antialiased bg-background text-foreground">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className="min-h-screen flex flex-col items-center">
+            <div className="flex-1 w-full flex flex-col gap-20 items-center">
+              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+                  <div className="flex gap-5 items-center font-semibold">
+                    <Link href={"/"}>Next.js Supabase Starter</Link>
+                    <div className="flex items-center gap-2">
+                      <DeployButton />
+                    </div>
+                  </div>
+                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+                </div>
+              </nav>
+              <div className="flex flex-col gap-20 max-w-5xl p-5">
+                {children}
+              </div>
+
+              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+                <p>
+                  Powered by{" "}
+                  <a
+                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+                    target="_blank"
+                    className="font-bold hover:underline"
+                    rel="noreferrer"
+                  >
+                    Supabase
+                  </a>
+                </p>
+                <ThemeSwitcher />
+              </footer>
+            </div>
+          </main>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <main className="flex-1 flex flex-col gap-6 px-4">
+      <h2 className="font-medium text-xl mb-4">Next steps</h2>
+      {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+    </main>
+  );
 }
